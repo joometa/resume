da176232750aee99f8ad8f105fced7e5febbce3a
joometa <jjlee1623@gmail.com>
2022-03-04 00:20:32 +0900

    Feat : Apply typewritter
    소개글 타이핑 효과 적용

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useRef } from 'react';
+import Typewriter from 'typewriter-effect';
 
 type IntroProps = {
   className?: string;
@@ -8,13 +8,41 @@ type IntroProps = {
 export const Intro = ({ className }: IntroProps) => {
   return (
     <section className="intro-container">
-      <h1 className="title">이정주</h1>
+      <div className="title">이정주</div>
       <div className="update">
         <sub>
           <i>Last updated: 2022.02.22</i>
         </sub>
       </div>
-      <p className="description">프론트엔드 개발자 입니다.</p>
+      <div className="description">
+        <Typewriter
+          options={{ autoStart: true, loop: true }}
+          onInit={(typewriter) => {
+            typewriter.typeString('안녕하세요. <br/><br/> 프론트엔드 개발자 이정주 입니다.').pauseFor(500).start();
+          }}
+        />
+      </div>
+
+      <table className="contact">
+        <tr>
+          <td className="type">GitHub</td>
+          <td className="info">
+            <a href="https://github.com/joometa">https://github.com/joometa</a>
+          </td>
+        </tr>
+        <tr>
+          <td className="type">Blog</td>
+          <td className="info">
+            <a href="https://data-jj.tistory.com/">https://data-jj.tistory.com</a>
+          </td>
+        </tr>
+        <tr>
+          <td className="type">Email</td>
+          <td className="info">
+            <a href="mailto:jjlee1623@gmail.com">jjlee1623@gmail.com</a>
+          </td>
+        </tr>
+      </table>
     </section>
   );
 };
